a9029296b5364e225b687c904893151569ff9879
Thompson Opeyemi <57995305+thompsondev@users.noreply.github.com>
2025-10-28 10:51:58 +0100

    update

---
+++ components/ui/dropdown-menu.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export {
+  Dropdown as DropdownMenu,
+  DropdownTrigger as DropdownMenuTrigger,
+  DropdownMenu as DropdownMenuContent,
+  DropdownItem as DropdownMenuItem,
+  DropdownSection as DropdownMenuGroup,
+} from "@heroui/react";
