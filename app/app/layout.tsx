7f011ea4b19d140140689e838c82c6ed98c593c3
Thompson Opeyemi <57995305+thompsondev@users.noreply.github.com>
2025-10-29 00:18:47 +0100

    update

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,14 +1,14 @@
-import { Navbar } from "@/components/reusables";
+// import { Navbar } from "@/components/reusables";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <section className="">
-      {children}
-      <Navbar />
-    </section>
-  );
-}
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <section className="">
+//       {children}
+//       <Navbar />
+//     </section>
+//   );
+// }
