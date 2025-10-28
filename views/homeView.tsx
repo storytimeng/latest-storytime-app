3d4c61342d5b2dd4da8317c93547831d2fc92fb4
Thompson Opeyemi <57995305+thompsondev@users.noreply.github.com>
2025-10-28 10:34:28 +0100

    update

--- views/homeView.tsx
+++ views/homeView.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Magnetik_Bold } from '@/lib/font'
 
 const HomeView = () => {
   return (
     <div className='w-full h-full text-center items-center justify-center'>
-        <h1 className='text-4xl font-bold text-red-700'>Welcome to the Home Page</h1>
-        <p className='text-lg text-blue-700'>This is the home page of the website</p>
+        <h1 className={`text-4xl font-bold text-red-700 ${Magnetik_Bold.className}`}>Welcome to the Home Page</h1>
+        <p className={`text-lg text-blue-700 ${Magnetik_Bold.className}`}>This is the home page of the website</p>
         
     </div>
   )
