bd353255db13bdefbdd4d0eb9da1e55bf1d47452
Thompson Opeyemi <57995305+thompsondev@users.noreply.github.com>
2025-10-28 23:50:38 +0100

    update

---
+++ app/app/notification/page.tsx
@@ -0,0 +1,8 @@
+import NotificationView from '@/views/notificationView';
+import React from 'react'
+
+const NotificationPage = () => {    
+  return <NotificationView />
+}
+
+export default NotificationPage
