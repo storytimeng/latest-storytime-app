6aee36767999442fa3e0c35582371b6b9563be86
Thompson Opeyemi <57995305+thompsondev@users.noreply.github.com>
2025-10-28 09:57:31 +0100

    updload foundation

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import HomeView from "@/views/homeView";
+
+export default function Home() {
+  return (
+   <HomeView />
+  );
+}
---
+++ views/homeView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeView = () => {
+  return (
+    <div className='w-full h-full text-center items-center justify-center'>
+        <h1 className='text-4xl font-bold text-red-700'>Welcome to the Home Page</h1>
+        <p className='text-lg text-blue-700'>This is the home page of the website</p>
+        
+    </div>
+  )
+}
+
+export default HomeView
