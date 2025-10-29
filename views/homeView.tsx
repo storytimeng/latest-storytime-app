87a9e895fa160a47d1793e75b0c6bf4601cd2464
Thompson Opeyemi <57995305+thompsondev@users.noreply.github.com>
2025-10-29 20:59:40 +0100

    update

--- views/homeView.tsx
+++ views/homeView.tsx
@@ -157,7 +157,7 @@ const HomeView = () => {
               height={500}
             />
             <div className="absolute top-2 right-2">
-              <div className="bg-accent-colour text-[10px] px-3 py-[3px] rounded-[2px] text-primary-colour">
+              <div className="bg-[#FFEBD0] text-[10px] px-3 py-[3px] rounded-[2px] text-[#361B17]">
                 Romance
               </div>
             </div>
@@ -174,7 +174,7 @@ const HomeView = () => {
                 price was too high. The beautiful lady wanted more than what
                 they were offering...
                 <span
-                  className={`text-[10px] font-bold text-complimentary-colour ${Magnetik_Bold.className}`}
+                  className={`text-[10px] font-bold text-[#F8951D] ${Magnetik_Bold.className}`}
                 >
                   by Jane Moore
                 </span>
