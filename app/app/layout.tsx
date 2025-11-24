9753fbf1a32346f554285188109afffc84860b89
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2025-11-24 14:14:02 +0100

    copied layout and page file of app folder

---
+++ app/app/layout.tsx
@@ -0,0 +1,9 @@
+//import { Navbar } from "@/components/reusables";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section className=" bg-accent-shade-1">{children}</section>;
+}
