35dc93883cce1843ac644a3fe464375999a0a940
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2026-06-29 19:03:23 +0100

    fix(android): remove invalid props from ReadStoryView in android override

--- android-pages/story/[id]/read/client.tsx
+++ android-pages/story/[id]/read/client.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import { ReadStoryView } from "@/views/app/story/readStoryView";
 
 export default function ReadStoryClient({ storyId }: { storyId: string }) {
-  const searchParams = useSearchParams();
-  const chapterId = searchParams.get("chapterId") ?? undefined;
-  const episodeId = searchParams.get("episodeId") ?? undefined;
-
-  return (
-    <ReadStoryView
-      storyId={storyId}
-      chapterId={chapterId}
-      episodeId={episodeId}
-    />
-  );
+  return <ReadStoryView storyId={storyId} />;
 }
